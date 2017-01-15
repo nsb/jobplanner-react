f3c8444400362cca71fce157115e6570339d142e
Niels Sandholt Busch <niels.busch@gmail.com>
2017-01-15 15:44:30 +0100

    fix token state

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,8 +11,7 @@ export const toggleNav = () => {
   }
 }
 
-export const requestLogin = (credentials) => {
-  const { username, password, rememberMe } = credentials
+export const requestLogin = (username, password, rememberMe) => {
 
   return {
     type: REQUEST_LOGIN,
@@ -25,12 +24,12 @@ export const requestLogin = (credentials) => {
 export const receiveLogin = (json) => {
   return {
     type: REQUEST_LOGIN_SUCCESS,
-    token: json.data.children.map(child => child.data),
+    token: json.token,
     receivedAt: Date.now()
   }
 }
 
-export const receiveLoginError = (json) => {
+export const receiveLoginError = (error) => {
   return {
     type: REQUEST_LOGIN_FAILURE,
     error: 'Oops'
@@ -39,6 +38,7 @@ export const receiveLoginError = (json) => {
 
 
 export const login = (credentials) => {
+  const { username, password, rememberMe } = credentials
   // Thunk middleware knows how to handle functions.
   // It passes the dispatch method as an argument to the function,
   // thus making it able to dispatch actions itself.
@@ -48,7 +48,7 @@ export const login = (credentials) => {
     // First dispatch: the app state is updated to inform
     // that the API call is starting.
 
-    dispatch(requestLogin(credentials))
+    dispatch(requestLogin(username, password, rememberMe))
 
     // The function called by the thunk middleware can return a value,
     // that is passed on as the return value of the dispatch method.
@@ -61,14 +61,18 @@ export const login = (credentials) => {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(...credentials)
+      body: JSON.stringify({
+        username,
+        password
+      })
     }).then(response => response.json())
-      .then(json =>
+      .then(json => {
 
         // We can dispatch many times!
         // Here, we update the app state with the results of the API call.
-
         dispatch(receiveLogin(json))
+      }).catch((error) =>
+        dispatch(receiveLoginError(error))
       )
 
       // In a real world app, you also want to
