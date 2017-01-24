e2941bc8000aa322215b7ca6af788948bd5e5720
Niels Sandholt Busch <niels.busch@gmail.com>
2017-01-24 22:59:10 +0100

    add nav menu items

--- src/components/NavSidebar.js
+++ src/components/NavSidebar.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from "react"
-import { Sidebar, Header, Title, Button, Footer } from 'grommet'
+import { Sidebar, Header, Title, Menu, Anchor, Button, Footer } from 'grommet'
 import CloseIcon from 'grommet/components/icons/base/Close'
 
 class NavSidebar extends Component {
@@ -18,6 +18,12 @@ class NavSidebar extends Component {
           <Button icon={<CloseIcon />} onClick={this.props.toggleNav} plain={true}
             a11yTitle="Close Menu" />
         </Header>
+        <Menu fill={true} primary={true}>
+          <Anchor key="dashboard" path="/dashboard" label="Dashboard" />
+          <Anchor key="calendar" path="/calendar" label="Calendar" />
+          <Anchor key="clients" path="/clients" label="Clients" />
+          <Anchor key="work" path="/work" label="Work" />
+        </Menu>
         <Footer pad={{horizontal: "medium", vertical: "small"}}>
         </Footer>
       </Sidebar>
