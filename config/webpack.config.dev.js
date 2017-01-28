89f46f7a3f6d5193bdbf39611a75e19191fd28be
Niels Sandholt Busch <niels.busch@gmail.com>
2017-01-28 14:12:55 +0100

    uncomment bundle size analyzer

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -205,8 +205,8 @@ module.exports = {
     new WatchMissingNodeModulesPlugin(paths.appNodeModules),
     new ServiceWorkerWebpackPlugin({
       entry: paths.serviceWorker,
-    }),
-    new BundleAnalyzerPlugin()
+    })
+    // new BundleAnalyzerPlugin()
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
