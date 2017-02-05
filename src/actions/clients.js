742e6d526f004a3806bb85f1701fdb2295d91bc9
Niels Sandholt Busch <niels.busch@gmail.com>
2017-02-05 12:27:13 +0100

    initial add client

---
+++ src/actions/clients.js
@@ -0,0 +1,72 @@
+import fetch from 'isomorphic-fetch'
+import { push } from 'react-router-redux'
+
+//Create new client
+export const CREATE_CLIENT = 'CREATE_CLIENT';
+export const CREATE_CLIENT_SUCCESS = 'CREATE_CLIENT_SUCCESS';
+export const CREATE_CLIENT_FAILURE = 'CREATE_CLIENT_FAILURE';
+export const RESET_NEW_CLIENT = 'RESET_NEW_CLIENT';
+
+export const createClientRequest = (client) => {
+
+  return {
+    type: CREATE_CLIENT,
+    client
+  }
+}
+
+export const createClientSuccess = (json) => {
+  return {
+    type: CREATE_CLIENT_SUCCESS,
+    client: json,
+    receivedAt: Date.now()
+  }
+}
+
+export const createClientError = (error) => {
+  return {
+    type: CREATE_CLIENT_FAILURE,
+    error: 'Oops'
+  }
+}
+
+
+export const createClient = (data, token) => {
+  // Thunk middleware knows how to handle functions.
+  // It passes the dispatch method as an argument to the function,
+  // thus making it able to dispatch actions itself.
+
+  return (dispatch) => {
+
+    // First dispatch: the app state is updated to inform
+    // that the API call is starting.
+
+    dispatch(createClientRequest(data))
+
+    // The function called by the thunk middleware can return a value,
+    // that is passed on as the return value of the dispatch method.
+
+    // In this case, we return a promise to wait for.
+    // This is not required by thunk middleware, but it is convenient for us.
+
+    return fetch('http://localhost:8000/clients/', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(Object.assign({}, data, {business: "/businesses/1/"}))
+    }).then(response => {
+      if (response.Ok) {
+        return response.json()
+      } else {
+        dispatch(createClientError('Oops'))
+      }
+    }).then(json => {
+          dispatch(createClientSuccess(json))
+          dispatch(push('/clients/1'))
+      }).catch((error) =>
+        dispatch(createClientError(error))
+      )
+  }
+}
--- src/components/ClientAdd.js
+++ src/components/ClientAdd.js
@@ -3,17 +3,18 @@ import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 import Article from 'grommet/components/Article'
 import ClientForm from './ClientForm'
+import { createClient } from '../actions'
 
 class ClientAdd extends Component {
   static propTypes = {
-    nav: PropTypes.object,
+    token: PropTypes.string.isRequired
   }
 
   render () {
     return (
       <Article align="center" pad={{horizontal: 'medium'}} primary={true}>
 
-        <ClientForm onSubmit={this.onSubmit}
+        <ClientForm onSubmit={this.handleSubmit}
           onClose={this.onClose} />
 
       </Article>
@@ -21,8 +22,9 @@ class ClientAdd extends Component {
 
   }
 
-  onSubmit = (data, dispatch, props) => {
-    console.log(data, props)
+  handleSubmit = (values) => {
+    const { token } = this.props
+    this.props.dispatch(createClient(values, token))
   }
 
   onClose = () => {
@@ -31,7 +33,9 @@ class ClientAdd extends Component {
 }
 
 const mapStateToProps = state => {
+  const { auth } = state
   return {
+    token: auth.token
   }
 }
 
