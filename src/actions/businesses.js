424ec6d2cb4f5563baacba134f007d7303601315
Niels Sandholt Busch <niels.busch@gmail.com>
2017-02-07 16:58:48 +0100

    remove comments

--- src/actions/businesses.js
+++ src/actions/businesses.js
@@ -32,30 +32,18 @@ export const createBusinessError = (error) => {
 
 
 export const createBusiness = (data, token) => {
-  // Thunk middleware knows how to handle functions.
-  // It passes the dispatch method as an argument to the function,
-  // thus making it able to dispatch actions itself.
 
   return (dispatch) => {
 
-    // First dispatch: the app state is updated to inform
-    // that the API call is starting.
-
     dispatch(createBusinessRequest(data))
-
-    // The function called by the thunk middleware can return a value,
-    // that is passed on as the return value of the dispatch method.
-
-    // In this case, we return a promise to wait for.
-    // This is not required by thunk middleware, but it is convenient for us.
 
     return businessesApi.createBusiness(data, token).then(responseBusiness => {
           dispatch(createBusinessSuccess(responseBusiness))
           dispatch(push(`/${responseBusiness.id}`))
           return responseBusiness
         }).catch(error => {
-          throw(error);
-        });
+          throw(error)
+        })
 
   }
 }
--- src/actions/clients.js
+++ src/actions/clients.js
@@ -32,31 +32,17 @@ export const createClientError = (error) => {
 
 
 export const createClient = (client, token) => {
-  // Thunk middleware knows how to handle functions.
-  // It passes the dispatch method as an argument to the function,
-  // thus making it able to dispatch actions itself.
 
   return (dispatch) => {
 
-    // First dispatch: the app state is updated to inform
-    // that the API call is starting.
-
     dispatch(createClientRequest(client))
-
-    // The function called by the thunk middleware can return a value,
-    // that is passed on as the return value of the dispatch method.
-
-    // In this case, we return a promise to wait for.
-    // This is not required by thunk middleware, but it is convenient for us.
-
 
     return clientsApi.createClient(client, token).then(responseClient => {
           dispatch(createClientSuccess(responseClient))
           dispatch(push(`/clients/${responseClient.id}`))
           return responseClient
         }).catch(error => {
-          throw(error);
-        });
-
+          throw(error)
+        })
   }
 }
