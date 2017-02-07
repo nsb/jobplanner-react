e5ba0dbbccb9784268f39ef0df5a053f3db21ef2
Niels Sandholt Busch <niels.busch@gmail.com>
2017-02-07 07:18:04 +0100

    add clients api

--- src/actions/clients.js
+++ src/actions/clients.js
@@ -1,5 +1,5 @@
-import fetch from 'isomorphic-fetch'
 import { push } from 'react-router-redux'
+import clientsApi from '../api/ClientsApi'
 
 //Create new client
 export const CREATE_CLIENT = 'CREATE_CLIENT';
@@ -31,7 +31,7 @@ export const createClientError = (error) => {
 }
 
 
-export const createClient = (data, token) => {
+export const createClient = (client, token) => {
   // Thunk middleware knows how to handle functions.
   // It passes the dispatch method as an argument to the function,
   // thus making it able to dispatch actions itself.
@@ -41,7 +41,7 @@ export const createClient = (data, token) => {
     // First dispatch: the app state is updated to inform
     // that the API call is starting.
 
-    dispatch(createClientRequest(data))
+    dispatch(createClientRequest(client))
 
     // The function called by the thunk middleware can return a value,
     // that is passed on as the return value of the dispatch method.
@@ -49,24 +49,33 @@ export const createClient = (data, token) => {
     // In this case, we return a promise to wait for.
     // This is not required by thunk middleware, but it is convenient for us.
 
-    return fetch('http://localhost:8000/clients/', {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(Object.assign({}, data, {business: "/businesses/1/"}))
-    }).then(response => {
-      if (response.Ok) {
-        return response.json()
-      } else {
-        dispatch(createClientError('Oops'))
-      }
-    }).then(json => {
-          dispatch(createClientSuccess(json))
-          dispatch(push('/clients/1'))
-      }).catch((error) =>
-        dispatch(createClientError(error))
-      )
+
+    return clientsApi.createClient(client, token).then(responseClient => {
+          dispatch(createClientSuccess(responseClient))
+          dispatch(push(`/clients/${responseClient.id}`))
+          return responseClient
+        }).catch(error => {
+          throw(error);
+        });
+
+  //   return fetch('http://localhost:8000/clients/', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Authorization': `Bearer ${token}`,
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(Object.assign({}, data, {business: "/businesses/1/"}))
+  //   }).then(response => {
+  //     if (response.Ok) {
+  //       return response.json()
+  //     } else {
+  //       dispatch(createClientError('Oops'))
+  //     }
+  //   }).then(json => {
+  //         dispatch(createClientSuccess(json))
+  //         dispatch(push('/clients/1'))
+  //     }).catch((error) =>
+  //       dispatch(createClientError(error))
+  //     )
   }
 }
---
+++ src/api/ClientsApi.js
@@ -0,0 +1,60 @@
+import fetch from 'isomorphic-fetch'
+
+class ClientsApi {
+  static getAllClients(token) {
+    return fetch('http://localhost:8000/clients').then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  }
+
+  static updateClient(client) {
+    const request = new Request(`http://localhost:8000/clients/${client.id}`, {
+      method: 'PUT',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify({client: client})
+    });
+
+
+    return fetch(request).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  }
+
+  static createClient(client, token) {
+    const request = new Request('http://localhost:8000/clients/', {
+      method: 'POST',
+      headers: new Headers({
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify(client)
+    });
+
+
+    return fetch(request).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  }
+
+  static deleteCat(cat) {
+    const request = new Request(`http://localhost:5000/api/v1/Clients/${cat.id}`, {
+      method: 'DELETE'
+    });
+
+    return fetch(request).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  }
+}
+
+export default ClientsApi;
