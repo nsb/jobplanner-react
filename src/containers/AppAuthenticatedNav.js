10a59ff16bf0ad2a0bbb2bf65df53d871a343141
Niels Sandholt Busch <niels.busch@gmail.com>
2017-02-12 19:11:51 +0100

    use find in mapstatetoprops

--- src/containers/AppAuthenticatedNav.js
+++ src/containers/AppAuthenticatedNav.js
@@ -8,16 +8,14 @@ class AppAuthenticatedNav extends Component {
   static propTypes = {
     navActive: PropTypes.bool.isRequired,
     responsive: PropTypes.string.isRequired,
-    businesses: PropTypes.array.isRequired
+    business: PropTypes.object.isRequired
   }
 
   render() {
     const { navActive, responsive } = this.props
     const priority = (navActive && 'single' === responsive ? 'left' : 'right')
 
-    const { businesses, params } = this.props
-    let business = businesses.find(
-      business => business.id === parseInt(params.businessId, 10))
+    const { business } = this.props
 
     return (
         <Split priority={priority} flex="right"
@@ -37,13 +35,13 @@ class AppAuthenticatedNav extends Component {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
   const { nav, businesses } = state
 
   return {
     navActive: nav.active,
     responsive: nav.responsive,
-    businesses: businesses.items
+    business: businesses.items.find(business => business.id === parseInt(ownProps.params.businessId, 10))
   }
 }
 
