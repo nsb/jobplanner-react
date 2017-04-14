2bd2482f49f5bd00e5f79ea47c2f0dfe72e261c4
Niels Sandholt Busch <niels.busch@gmail.com>
2017-04-14 22:57:44 +0200

    more types

--- src/actions/nav.js
+++ src/actions/nav.js
@@ -1,15 +1,30 @@
-export const NAV_ACTIVE = 'NAV_ACTIVE'
-export const NAV_TOGGLE = 'NAV_TOGGLE'
-export const NAV_RESPONSIVE = 'NAV_RESPONSIVE'
+// @flow
+export const NAV_ACTIVE: 'NAV_ACTIVE' = 'NAV_ACTIVE'
+export const NAV_TOGGLE: 'NAV_TOGGLE' = 'NAV_TOGGLE'
+export const NAV_RESPONSIVE: 'NAV_RESPONSIVE' = 'NAV_RESPONSIVE'
 
-export function navActivate (active) {
-  return { type: NAV_ACTIVE, active: active }
+type NavActiveAction = {
+  type: typeof NAV_ACTIVE,
+  active: boolean
 }
 
-export function navToggle (active) {
+type NavToggleAction = {
+  type: typeof NAV_TOGGLE
+}
+
+type NavResponseAction = {
+  type: typeof NAV_RESPONSIVE,
+  responsive: boolean
+}
+
+export const navActivate = (active: boolean): NavActiveAction => {
+  return { type: NAV_ACTIVE, active }
+}
+
+export const navToggle = (active: boolean): NavToggleAction => {
   return { type: NAV_TOGGLE }
 }
 
-export function navResponsive (responsive) {
+export function navResponsive (responsive: boolean): NavResponseAction {
   return { type: NAV_RESPONSIVE, responsive: responsive }
 }
