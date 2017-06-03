3b76e512cd02c64df862f594449532c3e8bc6d2e
Niels Sandholt Busch <niels.busch@gmail.com>
2017-06-03 18:06:44 +0200

    prettier

--- src/components/JobEdit.js
+++ src/components/JobEdit.js
@@ -1,15 +1,15 @@
 // @flow
 
-import React, {Component} from 'react';
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
-import Article from 'grommet/components/Article';
-import JobForm from './JobForm';
-import {updateJob} from '../actions/jobs';
-import type {State} from '../types/State';
-import type {State as ClientsState} from '../reducers/clients';
-import type {Business} from '../actions/businesses';
-import type {Job} from '../actions/jobs';
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import Article from "grommet/components/Article";
+import JobForm from "./JobForm";
+import { updateJob } from "../actions/jobs";
+import type { State } from "../types/State";
+import type { State as ClientsState } from "../reducers/clients";
+import type { Business } from "../actions/businesses";
+import type { Job } from "../actions/jobs";
 
 class JobEdit extends Component {
   props: {
@@ -18,18 +18,18 @@ class JobEdit extends Component {
     clients: ClientsState,
     updateJob: (d: Dispatch) => Promise<Job>,
     job: Job,
-    push: string => void,
+    push: string => void
   };
 
   render() {
-    const {clients, job} = this.props;
+    const { clients, job } = this.props;
     const client = clients.entities.clients[job.client];
     const clientList = clients.result.map(Id => {
       return clients.entities.clients[Id];
     });
 
     return (
-      <Article align="center" pad={{horizontal: 'medium'}} primary={true}>
+      <Article align="center" pad={{ horizontal: "medium" }} primary={true}>
 
         <JobForm
           onSubmit={this.handleSubmit}
@@ -37,7 +37,7 @@ class JobEdit extends Component {
           clients={clientList}
           initialValues={{
             ...job,
-            client: {label: client.first_name, value: client.id},
+            client: { label: client.first_name, value: client.id }
           }}
         />
 
@@ -47,21 +47,21 @@ class JobEdit extends Component {
 
   handleSubmit = values => {
     // get client Id
-    const {client: {value: clientId}} = values;
+    const { client: { value: clientId } } = values;
 
-    const {token, business, updateJob} = this.props;
+    const { token, business, updateJob } = this.props;
     updateJob(
       {
         ...values,
         business: business.id,
-        client: clientId,
+        client: clientId
       },
-      token || ''
+      token || ""
     );
   };
 
   onClose = () => {
-    const {business, push} = this.props;
+    const { business, push } = this.props;
     push(`/${business.id}/jobs`);
   };
 }
@@ -69,11 +69,11 @@ class JobEdit extends Component {
 const mapStateToProps = (
   state: State,
   ownProps: {
-    match: {params: {businessId: number, jobId: number}},
-    history: {push: string => void},
+    match: { params: { businessId: number, jobId: number } },
+    history: { push: string => void }
   }
 ) => {
-  const {auth, businesses, clients, jobs} = state;
+  const { auth, businesses, clients, jobs } = state;
   const businessId = parseInt(ownProps.match.params.businessId, 10);
   const jobId = parseInt(ownProps.match.params.jobId, 10);
 
@@ -82,14 +82,14 @@ const mapStateToProps = (
     business: businesses.entities.businesses[businessId],
     job: jobs.entities.jobs[jobId],
     push: ownProps.history.push,
-    clients,
+    clients
   };
 };
 
 const mapDispatchToProps = (dispatch: *) =>
   bindActionCreators(
     {
-      updateJob,
+      updateJob
     },
     dispatch
   );
