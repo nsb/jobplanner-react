b77452c251baf401a7dde01ec1ad203250ed1c48
Niels Sandholt Busch <niels.busch@gmail.com>
2017-09-08 15:05:30 +0200

    update react intl scripts

---
+++ scripts/translate.js
@@ -0,0 +1,22 @@
+// scripts/translate.js
+
+process.env.NODE_ENV = 'development';
+
+const fs = require('fs');
+const path = require('path');
+const globSync = require('glob').sync;
+const mkdirpSync = require('mkdirp').sync;
+const transformFileSync = require('babel-core').transformFileSync;
+
+const paths = {
+  appSrc: 'src',
+  appBuildMessages: 'build/messages'
+}
+
+globSync(path.join(paths.appSrc, '/**/*.js'))
+    .map((filename) => {
+        const result = transformFileSync(filename, {
+            plugins: [['react-intl',  {"messagesDir": paths.appBuildMessages}]],
+            presets: ['react-app', 'flow']
+        });
+    });
