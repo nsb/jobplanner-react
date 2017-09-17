ce47a0626881ef3f7fbf2da13e5907a8f6913a08
Niels Sandholt Busch <niels.busch@gmail.com>
2017-09-17 14:03:41 +0200

    use timestamp component

--- src/components/VisitListItem.js
+++ src/components/VisitListItem.js
@@ -1,34 +1,39 @@
 // @flow
 
-import React, {Component} from 'react';
-import moment from 'moment';
-import ListItem from 'grommet/components/ListItem';
-import CheckBox from 'grommet/components/CheckBox';
-import type {Visit} from '../actions/visits';
+import React, { Component } from "react";
+import moment from "moment";
+import ListItem from "grommet/components/ListItem";
+import CheckBox from "grommet/components/CheckBox";
+import Timestamp from "grommet/components/Timestamp";
+import type { Visit } from "../actions/visits";
 
 type Props = {
   visit: Visit,
   index: number,
-  onClick: SyntheticEvent<> => void,
+  onClick: (SyntheticEvent<>) => void
 };
 
 class VisitListItem extends Component<Props> {
-
   render() {
-    const {visit, index, onClick} = this.props;
+    const { visit, index, onClick } = this.props;
     return (
       <ListItem
         direction="row"
         align="center"
         justify="between"
-        separator={index === 0 ? 'horizontal' : 'bottom'}
-        pad={{horizontal: 'medium', vertical: 'small', between: 'medium'}}
+        separator={index === 0 ? "horizontal" : "bottom"}
+        pad={{ horizontal: "medium", vertical: "small", between: "medium" }}
         responsive={false}
         onClick={onClick}
         selected={false}
       >
-        <CheckBox label='' />
-        <span>{moment(visit.begins).format('l')}</span>
+        <CheckBox label="" />
+        <span>
+          <Timestamp
+            fields={visit.anytime ? "date" : ["date", "time"]}
+            value={visit.begins}
+          />
+        </span>
       </ListItem>
     );
   }
