5a442dc67013e030e43e87975f12d5ed62866682
Niels Sandholt Busch <niels.busch@gmail.com>
2017-10-10 09:34:39 +0200

    list line items

--- src/components/JobDetail.js
+++ src/components/JobDetail.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React, { Component } from "react";
+import { injectIntl, intlShape } from "react-intl";
 import Split from "grommet/components/Split";
 import Box from "grommet/components/Box";
 import Button from "grommet/components/Button";
@@ -10,6 +11,9 @@ import Header from "grommet/components/Header";
 import Heading from "grommet/components/Heading";
 import Section from "grommet/components/Section";
 import Columns from "grommet/components/Columns";
+import List from "grommet/components/List";
+import ListPlaceholder from "grommet-addons/components/ListPlaceholder";
+import ListItem from "grommet/components/ListItem";
 import MoreIcon from "grommet/components/icons/base/More";
 import LinkPreviousIcon from "grommet/components/icons/base/LinkPrevious";
 import { rrulestr } from "rrule";
@@ -30,7 +34,8 @@ type Props = {
   onRemove: Function,
   onToggleCloseJob: Function,
   onResponsive: Function,
-  saved: boolean
+  saved: boolean,
+  intl: intlShape
 };
 
 type State = {
@@ -52,7 +57,8 @@ class JobDetail extends Component<Props, State> {
       onEdit,
       onRemove,
       onResponsive,
-      onToggleCloseJob
+      onToggleCloseJob,
+      intl
     } = this.props;
 
     let onSidebarClose;
@@ -140,17 +146,41 @@ class JobDetail extends Component<Props, State> {
                   </Box>
                 </Columns>
               </Section>
-              <Section pad="medium" full="horizontal">
-                <Heading tag="h4" margin="none">
-                  Lineitems
-                </Heading>
-                {lineItems.map((line_item, index) =>
-                  <div>{line_item.name} {index}</div>
-                )}
-
+              <Section full="horizontal">
+                <Box pad={{ horizontal: "medium", vertical: "none" }}>
+                  <Heading tag="h4">
+                    Line items
+                  </Heading>
+                </Box>
+                <Box>
+                  <List onMore={undefined}>
+                    {lineItems.map((line_item, index) =>
+                      <ListItem
+                        direction="row"
+                        align="center"
+                        justify="between"
+                        separator={index === 0 ? "horizontal" : "bottom"}
+                        pad={{ horizontal: "medium", vertical: "small", between: "medium" }}
+                        responsive={false}
+                        onClick={undefined}
+                        selected={false}
+                      >
+                        {line_item.name} {index}
+                      </ListItem>
+                    )}
+                  </List>
+                  <ListPlaceholder
+                    filteredTotal={lineItems.length}
+                    unfilteredTotal={lineItems.length}
+                    emptyMessage={intl.formatMessage({
+                      id: "lineItems.emptyMessage",
+                      defaultMessage: "No line items."
+                    })}
+                  />
+                </Box>
               </Section>
               <Section full="horizontal">
-                <Box pad={{ horizontal: "medium", vertial: "none" }}>
+                <Box pad={{ horizontal: "medium", vertical: "none" }}>
                   <Heading tag="h4">
                     Visits
                   </Heading>
@@ -172,4 +202,4 @@ class JobDetail extends Component<Props, State> {
   };
 }
 
-export default JobDetail;
+export default injectIntl(JobDetail);
