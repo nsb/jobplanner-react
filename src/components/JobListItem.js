6fafd7e382ffdddf51c4950bcb37078ba687c77a
Niels Sandholt Busch <niels.busch@gmail.com>
2017-11-25 21:23:50 +0100

    show job number and dates in job list

--- src/components/JobListItem.js
+++ src/components/JobListItem.js
@@ -1,31 +1,35 @@
 // @flow
 
-import React, {Component} from 'react';
-import ListItem from 'grommet/components/ListItem';
-import type {Job} from '../actions/jobs';
+import React, { Component } from "react";
+import ListItem from "grommet/components/ListItem";
+import Timestamp from "grommet/components/Timestamp";
+import type { Job } from "../actions/jobs";
 
 type Props = {
   job: Job,
   index: number,
-  onClick: Function,
+  onClick: Function
 };
 
 class JobListItem extends Component<Props> {
-
   render() {
-    const {job, index, onClick} = this.props;
+    const { job, index, onClick } = this.props;
     return (
       <ListItem
         direction="row"
         align="center"
         justify="between"
-        separator={index === 0 ? 'horizontal' : 'bottom'}
-        pad={{horizontal: 'medium', vertical: 'small', between: 'medium'}}
+        separator={index === 0 ? "horizontal" : "bottom"}
+        pad={{ horizontal: "medium", vertical: "small", between: "medium" }}
         responsive={false}
         onClick={onClick}
         selected={false}
       >
-        <span>{job.description}</span>
+        <span>Job #{job.id}</span>
+        <span>
+          <Timestamp value={job.begins} fields="date" /> -{" "}
+          <Timestamp value={job.ends} fields="date" />
+        </span>
       </ListItem>
     );
   }
