52d1d1f1eaa455588a63f36c69ce16148d9447c2
Niels Sandholt Busch <niels.busch@gmail.com>
2017-11-25 15:35:11 +0100

    show client phone or email

--- src/components/ClientListItem.js
+++ src/components/ClientListItem.js
@@ -1,31 +1,35 @@
 // @flow
 
-import React, {Component} from 'react';
-import ListItem from 'grommet/components/ListItem';
-import UserIcon from 'grommet/components/icons/base/User';
-import type {Client} from '../actions/clients';
+import React, { Component } from "react";
+import ListItem from "grommet/components/ListItem";
+import UserIcon from "grommet/components/icons/base/User";
+import type { Client } from "../actions/clients";
 
 type Props = {
   client: Client,
   index: number,
-  onClick: SyntheticInputEvent<*> => void,
+  onClick: (SyntheticInputEvent<*>) => void
 };
 
 class ClientListItem extends Component<Props> {
   render() {
-    const {client, index, onClick} = this.props;
+    const { client, index, onClick } = this.props;
     return (
       <ListItem
         direction="row"
         align="center"
         justify="between"
-        separator={index === 0 ? 'horizontal' : 'bottom'}
-        pad={{horizontal: 'medium', vertical: 'small', between: 'medium'}}
+        separator={index === 0 ? "horizontal" : "bottom"}
+        pad={{ horizontal: "medium", vertical: "small", between: "medium" }}
         responsive={false}
         onClick={onClick}
         selected={false}
       >
-        <span><UserIcon type="logo" size="xsmall" /> {client.first_name} {client.last_name}</span>
+        <span>
+          <UserIcon type="logo" size="xsmall" /> {client.first_name}{" "}
+          {client.last_name}
+        </span>
+        <span>{client.phone || client.email}</span>
       </ListItem>
     );
   }
