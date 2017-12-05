cc4f9118d4d08308c3e4e4feed64f6de50a07583
Niels Sandholt Busch <niels.busch@gmail.com>
2017-12-05 21:03:41 +0100

    sort visits

--- src/selectors/visitSelectors.js
+++ src/selectors/visitSelectors.js
@@ -19,7 +19,11 @@ const getJob = (state: ReduxState, props: { job: Job }): Job => {
 export const getVisitsByJob: Function = createSelector(
   [getVisits, getJob],
   (visits: Array<Visit>, job: Job): Array<Visit> => {
-    return visits.filter(visit => visit.job === job.id);
+    return visits
+      .filter(visit => visit.job === job.id)
+      .sort((a, b) => {
+        return new Date(a.begins) - new Date(b.begins);
+      })
   }
 );
 
