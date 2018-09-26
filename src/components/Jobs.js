b2f19f93b0651a9201c4b4c218942e29226d91ec
Niels Sandholt Busch <niels.busch@gmail.com>
2018-09-26 19:18:39 +0200

    No need to set locale in jobs. It's set globally in index.

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,24 +1,11 @@
 // @flow
 
 import React, { Component } from "react";
-import moment from "moment";
-import "moment/locale/da";
 import { Switch, Route } from "react-router-dom";
 import JobList from "../components/JobListRouterContainer";
 import JobsAdd from "../components/JobsAdd";
 import JobDetailContainer from "../components/JobDetailContainer";
 import JobEdit from "../components/JobEdit";
-
-const language =
-  (navigator.languages && navigator.languages[0]) || navigator.language;
-
-// configure moment locale, with Monday as 1st day of week
-moment.locale(language, {
-  week: {
-    dow: 1,
-    doy: 1
-  }
-});
 
 class Jobs extends Component<*> {
   render() {
