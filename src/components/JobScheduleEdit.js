139479951c89d902d39908c8a8fbf1ef9025633f
Niels Sandholt Busch <niels.busch@gmail.com>
2018-09-27 15:35:57 +0200

    Remove comments.

--- src/components/JobScheduleEdit.js
+++ src/components/JobScheduleEdit.js
@@ -35,7 +35,6 @@ const rruleByWeekDay = [
   { label: "Sunday", value: RRule.SU.weekday }
 ];
 
-// [1, 2, 3, 4, 5, 6, 7][8, 9, 10, 11, 12, 13, 14][15, 16, 17, 18, 19, 20, 21][22, 23, 24, 25, 26, 27, 28][29, 30, -1]
 const rruleByMonthDay = [
   { label: "First", value: 1 },
   { label: "Second", value: 2 },
@@ -70,8 +69,6 @@ class JobScheduleEdit extends Component<Props, State> {
       }
     });
   }
-
-  // componentDidUpdate(prevProps) {}
 
   render() {
     const { onClose } = this.props;
