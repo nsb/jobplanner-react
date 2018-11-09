1bda93111b7b0e82443982bba97d8d4b3fbf26ec
Niels Sandholt Busch <niels.busch@gmail.com>
2018-11-09 13:08:45 +0100

    Add directions to visit

--- src/actions/properties.js
+++ src/actions/properties.js
@@ -1,7 +1,10 @@
 export type Property = {
   id: number,
   address1: string,
-  address2: string
+  address2: string,
+  city: string,
+  zip_code: string,
+  country: string
 };
 
 export type PropertiesMap = { [id: number]: Property };
--- src/components/VisitDetail.js
+++ src/components/VisitDetail.js
@@ -11,6 +11,7 @@ import Anchor from "grommet/components/Anchor";
 import Menu from "grommet/components/Menu";
 import ScheduleIcon from "grommet/components/icons/base/Schedule";
 import ActionsIcon from "grommet/components/icons/base/Actions";
+import DirectionsIcon from "grommet/components/icons/base/Directions";
 import type { Visit } from "../actions/visits";
 import type { Property } from "../actions/properties";
 import type { LineItem } from "../actions/lineitems";
@@ -31,6 +32,28 @@ class VisitDetail extends Component<Props> {
   render() {
     const { visit, property, assigned, lineItems, onEdit, onDelete } = this.props;
 
+    let schedule;
+    schedule = (
+      <Anchor icon={<ScheduleIcon />}
+        label={
+          <Timestamp
+            fields={visit.anytime ? "date" : ["date", "time"]}
+            value={visit.begins}
+          />}
+      />)
+
+    let directions;
+    let directionsParams = new URLSearchParams({
+      q: `${property.address1}, ${property.city}, ${property.zip_code}, ${property.country}`
+    });
+    directions = (
+      <Anchor icon={<DirectionsIcon />}
+        href={`https://maps.google.com/?${directionsParams.toString()}`}
+        target='_blank'
+        primary={true} >
+        Directions
+      </Anchor >)
+
     return (
       <Box>
         <Header justify="between" pad="none">
@@ -39,41 +62,38 @@ class VisitDetail extends Component<Props> {
           </Heading>
         </Header>
 
-        <Section pad="medium">
+        <Section pad="none">
           <Columns masonry={false} maxCount={2}>
-            <Box>
+            <Box pad={{ horizontal: "none", vertical: "none" }}>
               <Heading tag="h2" strong={true}>
                 {visit.client_name}
               </Heading>
             </Box>
-            <Box>
+            <Box pad={{ horizontal: "none", vertical: "small" }}>
+              {schedule}
+              {directions}
+              {visit.client_phone}
+            </Box>
+            <Box pad={{ horizontal: "none", vertical: "small" }}>
               {visit.client_name}
               <br />
               {property.address1}
               <br />
               {property.address2}
               <br />
+              {property.zip_code} {property.city}
             </Box>
-            <Box>
-              <Heading tag="h4">
-                <Box direction="row">
-                  <ScheduleIcon />
-                  <Timestamp
-                    fields={visit.anytime ? "date" : ["date", "time"]}
-                    value={visit.begins}
-                  />
-                </Box>
-              </Heading>
+            <Box pad={{ horizontal: "none", vertical: "small" }}>
+              {visit.details}
             </Box>
-            <Box margin="small">{visit.client_phone}</Box>
-            <Box>
+            <Box pad={{ horizontal: "none", vertical: "small" }}>
               <Menu
                 responsive={true}
                 inline={false}
                 primary={false}
                 label="Actions"
                 icon={<ActionsIcon />}
-              >
+                Directions >
                 <Anchor href="#" onClick={onEdit}>
                   Edit
                 </Anchor>
@@ -85,13 +105,13 @@ class VisitDetail extends Component<Props> {
                 </Anchor>
               </Menu>
             </Box>
-            <Box colorIndex="light-2" pad="small" margin="small">
+            <Box colorIndex="light-2" pad={{ horizontal: "small", vertical: "small" }}>
               <Heading tag="h4" strong={true}>
                 <Box direction="row">Job</Box>
               </Heading>
               Job #{visit.job}
             </Box>
-            <Box colorIndex="light-2" pad="small" margin="small">
+            <Box colorIndex="light-2" pad={{ horizontal: "small", vertical: "small" }}>
               <Heading tag="h4" strong={true}>
                 <Box direction="row">Team</Box>
               </Heading>
@@ -100,11 +120,11 @@ class VisitDetail extends Component<Props> {
                   return <div>{employee.username}</div>;
                 })
               ) : (
-                <div>Unassigned</div>
-              )}
+                  <div>Unassigned</div>
+                )}
             </Box>
           </Columns>
-          <Box>
+          <Box pad={{ horizontal: "none", vertical: "small" }}>
             <Heading tag="h4" strong={true}>
               <Box direction="row">Line Items</Box>
             </Heading>
@@ -119,7 +139,7 @@ class VisitDetail extends Component<Props> {
 
   toggleCompleted = () => {
     const { visit, token, partialUpdateVisit, onClose } = this.props;
-    if(token) {
+    if (token) {
       partialUpdateVisit({ id: visit.id, completed: !visit.completed }, token);
       onClose();
     }
