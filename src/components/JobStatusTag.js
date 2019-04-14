fe6237e4d85c4de778f51974ce25a885a26da8ff
Niels Sandholt Busch <niels.busch@gmail.com>
2019-04-14 15:24:32 +0200

    Always return from render

--- src/components/JobStatusTag.js
+++ src/components/JobStatusTag.js
@@ -16,7 +16,7 @@ class JobStatusTag extends Component<Props> {
             case 'today': return <Tag text="Today" color="neutral-1" />;
             case 'upcoming': return <Tag text="Upcoming" color="neutral-2" />;
             case 'archived': return <Tag text="Archived" color="unknown" />;
-            default:
+            default: return null
           }
     }
 }
