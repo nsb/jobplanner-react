490c4ecc83838a2e503607665f24cac1f2ac0346
Niels Sandholt Busch <niels.busch@gmail.com>
2019-06-29 19:19:45 +0200

    Translations

--- src/components/SettingsEmployeeForm.js
+++ src/components/SettingsEmployeeForm.js
@@ -1,19 +1,69 @@
 // @flow
 
 import React from "react";
+import {injectIntl, FormattedMessage} from 'react-intl';
 import { Field, reduxForm } from "redux-form";
 import Button from "grommet/components/Button";
 import Form from "grommet/components/Form";
 import Footer from "grommet/components/Footer";
 import FormFields from "grommet/components/FormFields";
 import FormField from "grommet/components/FormField";
+import { intlFormFieldRequired } from "../i18n";
 import type { Element } from "react";
 import type { Employee } from "../actions/employees";
+
+const intlUsername = (
+  <FormattedMessage
+    id="settingsEmployeeForm.usernameLabel"
+    description="Settings employee form username label"
+    defaultMessage="Username"
+  />
+)
+
+const intlFirstName = (
+  <FormattedMessage
+    id="settingsEmployeeForm.firstNameLabel"
+    description="Settings employee form first name label"
+    defaultMessage="First name"
+  />
+)
+
+const intlLastName = (
+  <FormattedMessage
+    id="settingsEmployeeForm.lastNameLabel"
+    description="Settings employee form last name label"
+    defaultMessage="Last name"
+  />
+)
+
+const intlUpdate = (
+  <FormattedMessage
+    id="settingsEmployeeForm.updateLabel"
+    description="Settings employee form update label"
+    defaultMessage="Update employee"
+  />
+)
+
+const intlAdd = (
+  <FormattedMessage
+    id="settingsEmployeeForm.addLabel"
+    description="Settings employee form add label"
+    defaultMessage="Add employee"
+  />
+)
+
+const intlEmail = (
+  <FormattedMessage
+    id="settingsEmployeeForm.emailLabel"
+    description="Settings employee form email label"
+    defaultMessage="E-mail"
+  />
+)
 
 const validate = (values: Object): Object => {
   const errors = {};
   if (!values.username) {
-    errors.username = "Required";
+    errors.username = intlFormFieldRequired;
   }
   return errors;
 };
@@ -52,25 +102,25 @@ export const EmployeeForm = ({
         <fieldset>
           <Field
             name="username"
-            label="Username"
+            label={intlUsername}
             component={renderField}
             type="text"
           />
           <Field
             name="first_name"
-            label="First name"
+            label={intlFirstName}
             component={renderField}
             type="text"
           />
           <Field
             name="last_name"
-            label="Last name"
+            label={intlLastName}
             component={renderField}
             type="text"
           />
           <Field
             name="email"
-            label="Email"
+            label={intlEmail}
             component={renderField}
             type="text"
           />
@@ -83,7 +133,7 @@ export const EmployeeForm = ({
           type="submit"
           primary={true}
           label={
-            initialValues && initialValues.id ? "Update employee" : "Add employee"
+            initialValues && initialValues.id ? intlUpdate : intlAdd
           }
           onClick={valid && dirty && !submitting ? () => true : undefined}
         />
@@ -94,4 +144,4 @@ export const EmployeeForm = ({
 
 export default reduxForm({
   validate
-})(EmployeeForm);
+})(injectIntl(EmployeeForm));
--- src/components/SettingsEmployeesSection.js
+++ src/components/SettingsEmployeesSection.js
@@ -1,9 +1,34 @@
 // @flow
 
 import React from "react";
+import {injectIntl, FormattedMessage} from 'react-intl';
 import Button from "grommet/components/Button";
 import EditIcon from "grommet/components/icons/base/Edit";
 import SettingsListItem from "./SettingsListItem";
+
+const intlHeading = (
+  <FormattedMessage
+    id="settingsEmployeeSection.Heading"
+    description="Settings employees heading"
+    defaultMessage="Employees"
+  />
+)
+
+const intlDetails = (
+  <FormattedMessage
+    id="settingsEmployeeSection.Details"
+    description="Settings employees details"
+    defaultMessage="These are your employees."
+  />
+)
+
+const intlEdit = (
+  <FormattedMessage
+    id="settingsEmployeeSection.Edit"
+    description="Settings employees edit"
+    defaultMessage="Edit employees"
+  />
+)
 
 type Props = {
   onOpen: Function,
@@ -13,21 +38,16 @@ const EmployeesSection = (props: Props) => {
   const { onOpen } = props;
   let control;
   control = (
-    <Button icon={<EditIcon />} onClick={onOpen} a11yTitle="Edit Employees" />
+    <Button icon={<EditIcon />} onClick={onOpen} a11yTitle={intlEdit} />
   );
 
-  let details;
-  details = (
-    <span>
-      These are your employees.
-    </span>
-  );
+
   return (
     <SettingsListItem key="employees" control={control} first={true}>
-      <strong>Employees</strong>
-      {details}
+      <strong>{intlHeading}</strong>
+      {intlDetails}
     </SettingsListItem>
   );
 };
 
-export default EmployeesSection;
+export default injectIntl(EmployeesSection);
