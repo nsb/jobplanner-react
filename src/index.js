1a306f84c45b2d83b4262496514e81f9f8bbff22
Niels Sandholt Busch <niels.busch@gmail.com>
2019-07-28 10:34:52 +0200

    Remove service worker code, not used

--- src/index.js
+++ src/index.js
@@ -48,27 +48,6 @@ if (process.env.REACT_APP_SENTRY_PUBLIC_DSN) {
   }).install();
 }
 
-// Setup service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    if (navigator.serviceWorker) {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then(function(registration) {
-          // Registration was successful
-          console.log(
-            "ServiceWorker registration successful with scope: ",
-            registration.scope
-          );
-        })
-        .catch(function(err) {
-          // registration failed :(
-          console.log("ServiceWorker registration failed: ", err);
-        });
-    }
-  });
-}
-
 // const NetworkListenerProvider = NetworkListener(Provider);
 
 // Create an enhanced history that syncs navigation events with the store
