b2c7a3824b72243138f600b9b94743abb475de85
Niels Sandholt Busch <niels.busch@gmail.com>
2019-07-31 15:22:49 +0200

    Improve search for clients

--- src/components/ClientList.js
+++ src/components/ClientList.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import {injectIntl, intlShape, FormattedMessage} from 'react-intl';
 import Box from 'grommet/components/Box';
 import Header from 'grommet/components/Header';
@@ -47,74 +47,135 @@ const intlEmptyMessage = (
   />
 )
 
-type Props = {
+export type Props = {
   business: Business,
-  clients?: Array<Client>,
-  isFetching?: boolean,
-  searchText?: string,
-  onSearch: SyntheticInputEvent<*> => void,
-  onMore: () => void,
-  onClick: (SyntheticInputEvent<*>, Client) => void,
-  addClient: SyntheticInputEvent<*> => void,
-  intl: intlShape,
+  clients: Array<Client>,
+  isFetching: boolean,
+  push: Function,
+  totalCount: number,
+  token: String
 };
 
-const ClientList = ({
-  business,
-  clients = [],
-  isFetching = false,
-  searchText = '',
-  onSearch,
-  onMore,
-  onClick,
-  addClient,
-  intl,
-}: Props) => (
-  <Box>
-    <Header size="large" pad={{horizontal: 'medium'}}>
-      <NavControl title={intlTitle} />
-      <Search
-        inline={true}
-        fill={true}
-        size="medium"
-        placeHolder={intl.formatMessage({id: 'clients.search'})}
-        value={searchText}
-        onDOMChange={onSearch}
-      />
-      <Anchor
-        icon={<AddIcon />}
-        path={`/${business.id}/clients/add`}
-        a11yTitle={intlAdd}
-      />
+type State = {
+  searchText: string,
+  searchResults: Array<number>,
+  offset: number,
+  limit: number
+};
 
-    </Header>
-    <List onMore={isFetching ? undefined : onMore}>
-      {clients.map((client: Client, index: number) => {
-        return (
-          <ClientListItem
-            key={client.id}
-            client={client}
-            index={index}
-            onClick={e => onClick(e, client)}
-          />
-        );
-      })}
-    </List>
-    <ListPlaceholder
-      filteredTotal={isFetching ? null : clients.length}
-      unfilteredTotal={isFetching ? null : clients.length}
-      emptyMessage={intlEmptyMessage}
-      addControl={
-        <Button
-          icon={<AddIcon />}
-          label={intlAdd}
-          primary={true}
-          a11yTitle={intlAdd}
-          path={`/${business.id}/clients/add`}
-        />
+class ClientList extends Component<Props & { intl: intlShape }, State> {
+  searchTimeout: number = null;
+  state: State = {
+    searchText: "",
+    searchResults: [],
+    offset: 0,
+    limit: 15
+  };
+
+  componentDidMount() {
+    this.onMore();
+  }
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    if (prevState.searchText !== this.state.searchText) {
+      clearTimeout(this.searchTimeout);
+      this.searchTimeout = setTimeout(() => {
+        this.setState({ offset: 0, searchResults: [] }, this.onMore);
+      }, 500);
+    }
+  }
+
+  render() {
+    const { business, clients, isFetching, intl, totalCount } = this.props;
+
+    const filteredClients = clients.filter(client => {
+      if (this.state.searchText) {
+        return this.state.searchResults.includes(client.id)
+      } else {
+        return true;
       }
-    />
-  </Box>
-);
+    });
+
+    return (
+      <Box>
+        <Header size="large" pad={{horizontal: 'medium'}}>
+          <NavControl title={intlTitle} />
+          <Search
+            inline={true}
+            fill={true}
+            size="medium"
+            placeHolder={intl.formatMessage({id: 'clients.search'})}
+            value={this.state.searchText}
+            onDOMChange={this.onSearch}
+          />
+          <Anchor
+            icon={<AddIcon />}
+            path={`/${business.id}/clients/add`}
+            a11yTitle={intlAdd}
+          />
+
+        </Header>
+        <List onMore={isFetching || this.state.offset > totalCount ? undefined : this.onMore}>
+          {filteredClients.map((client: Client, index: number) => {
+            return (
+              <ClientListItem
+                key={client.id}
+                client={client}
+                index={index}
+                onClick={e => this.onClick(e, client)}
+              />
+            );
+          })}
+        </List>
+        <ListPlaceholder
+          filteredTotal={isFetching ? null : filteredClients.length}
+          unfilteredTotal={isFetching ? null : this.state.searchText ? clients.length : totalCount}
+          emptyMessage={intlEmptyMessage}
+          addControl={
+            <Button
+              icon={<AddIcon />}
+              label={intlAdd}
+              primary={true}
+              a11yTitle={intlAdd}
+              path={`/${business.id}/clients/add`}
+            />
+          }
+        />
+      </Box>
+    );
+  }
+
+  onMore = () => {
+    const { business, token, fetchClients } = this.props;
+    if (token) {
+      fetchClients(token, {
+        business: business.id,
+        ordering: "first_name,last_name",
+        limit: this.state.limit,
+        offset: this.state.offset,
+        search: this.state.searchText
+      }).then(resultClients => {
+        this.setState({
+          offset: this.state.offset + this.state.limit,
+          searchResults: resultClients.results.map(client => client.id)
+        });
+      });
+    }
+  };
+
+  onClick = (e: SyntheticInputEvent<*>, client: Client) => {
+    const { push, business } = this.props;
+    push(`/${business.id}/clients/${client.id}`);
+  };
+
+  onSearch = ({ target }: SyntheticInputEvent<*>) => {
+    this.setState({ searchText: target.value, offset: 0 });
+  };
+
+  addClient = (e: SyntheticInputEvent<*>) => {
+    const { push, business } = this.props;
+    push(`/${business.id}/clients/add`);
+  };
+}
 
 export default injectIntl(ClientList);
--- src/components/ClientListContainer.js
+++ src/components/ClientListContainer.js
@@ -1,104 +1,18 @@
 // @flow
-import React, { Component } from "react";
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { fetchClients } from "../actions/clients";
 import ClientList from "./ClientList";
 import type { Dispatch } from "../types/Store";
 import type { State as ReduxState } from "../types/State";
-import type { Client } from "../actions/clients";
-import type { Business } from "../actions/businesses";
+import type { Props } from "./ClientList";
 import { ensureState } from "redux-optimistic-ui";
-
-type Props = {
-  business: Business,
-  clients: Array<Client>,
-  token: ?string,
-  isFetching: boolean,
-  dispatch: Dispatch,
-  push: string => void,
-  totalCount: number
-};
-
-type State = {
-  searchText: string,
-  offset: number,
-  limit: number
-};
-
-class ClientListContainer extends Component<Props, State> {
-  state: State = {
-    searchText: "",
-    offset: 0,
-    limit: 25
-  };
-
-  componentDidMount() {
-    this.onMore();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { dispatch } = this.props;
-
-    if (prevState.searchText !== this.state.searchText) {
-      dispatch({ type: "RESET_CLIENTS" });
-      this.onMore();
-    }
-  }
-
-  render() {
-    const { business, clients, isFetching, totalCount } = this.props;
-
-    return (
-      <ClientList
-        business={business}
-        clients={clients}
-        isFetching={isFetching}
-        onMore={this.state.offset < totalCount ? this.onMore : null}
-        onSearch={this.onSearch}
-        searchText={this.state.searchText}
-        onClick={this.onClick}
-        addClient={this.addClient}
-      />
-    );
-  }
-
-  onMore = () => {
-    const { business, token, dispatch } = this.props;
-    if (token) {
-      dispatch(
-        fetchClients(token, {
-          business: business.id,
-          ordering: "first_name",
-          limit: this.state.limit,
-          offset: this.state.offset,
-          search: this.state.searchText
-        })
-      );
-      this.setState({ offset: this.state.offset + this.state.limit });
-    }
-  };
-
-  onClick = (e: SyntheticInputEvent<*>, client: Client) => {
-    const { push, business } = this.props;
-    push(`/${business.id}/clients/${client.id}`);
-  };
-
-  onSearch = ({ target }: SyntheticInputEvent<*>) => {
-    this.setState({ searchText: target.value, offset: 0 });
-  };
-
-  addClient = (e: SyntheticInputEvent<*>) => {
-    const { push, business } = this.props;
-    push(`/${business.id}/clients/add`);
-  };
-}
 
 const mapStateToProps = (
   state: ReduxState,
   ownProps: {
     match: { params: { businessId: number } },
     history: { push: Function },
-    dispatch: Dispatch
   }
 ): Props => {
   const { clients, entities, auth } = state;
@@ -111,10 +25,17 @@ const mapStateToProps = (
     }),
     isFetching: clients.isFetching,
     token: auth.token,
-    dispatch: ownProps.dispatch,
     push: ownProps.history.push,
     totalCount: clients.count
   };
 };
 
-export default connect(mapStateToProps)(ClientListContainer);
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(
+    {
+      fetchClients
+    },
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClientList);
