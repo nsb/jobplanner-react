c4360feae50e170a2cdc83681fc2bc8883c6f737
Niels Sandholt Busch <niels.busch@gmail.com>
2019-11-29 22:28:05 +0100

    Use functional component

--- src/components/businessListItem.js
+++ src/components/businessListItem.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component } from "react";
+import React from "react";
 import ListItem from "grommet/components/ListItem";
 import type { Business } from "../actions/businesses";
 
@@ -10,25 +10,16 @@ type Props = {
   onClick: Function
 };
 
-class BusinessListItem extends Component<Props> {
-
-  render() {
-    const { business, index, onClick } = this.props;
-    return (
-      <ListItem
-        direction="row"
-        align="center"
-        justify="between"
-        separator={index === 0 ? "horizontal" : "bottom"}
-        pad={{ horizontal: "medium", vertical: "small", between: "medium" }}
-        responsive={false}
-        onClick={onClick}
-        selected={false}
-      >
-        <span>{business.name}</span>
-      </ListItem>
-    );
-  }
-}
-
-export default BusinessListItem;
+export default ({ business, index, onClick }: Props) =>
+  <ListItem
+    direction="row"
+    align="center"
+    justify="between"
+    separator={index === 0 ? "horizontal" : "bottom"}
+    pad={{ horizontal: "medium", vertical: "small", between: "medium" }}
+    responsive={false}
+    onClick={onClick}
+    selected={false}
+  >
+    <span>{business.name}</span>
+  </ListItem>
