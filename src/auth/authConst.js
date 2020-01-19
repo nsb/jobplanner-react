271692bba054307dd004113f854135d07bbbe0e8
Niels Sandholt Busch <niels.busch@gmail.com>
2020-01-19 13:33:33 +0100

    default values

--- src/auth/authConst.js
+++ src/auth/authConst.js
@@ -5,7 +5,7 @@ export const IDENTITY_CONFIG = {
   login: process.env.REACT_APP_AUTH_URL + "/login",
   automaticSilentRenew: true, //(boolean, default: false): Flag to indicate if there should be an automatic attempt to renew the access token prior to its expiration.
   loadUserInfo: false, //(boolean, default: true): Flag to control if additional identity data is loaded from the user info endpoint in order to populate the user's profile.
-  silent_redirect_uri: process.env.REACT_APP_SILENT_REDIRECT_URL || "http://localhost:3000/silentrenew", //(string): The URL for the page containing the code handling the silent renew.
+  silent_redirect_uri: process.env.REACT_APP_SILENT_REDIRECT_URL, //(string): The URL for the page containing the code handling the silent renew.
   post_logout_redirect_uri: process.env.REACT_APP_LOGOFF_REDIRECT_URL, // (string): The OIDC post-logout redirect URI.
   audience: "https://example.com", //is there a way to specific the audience when making the jwt
   response_type: "token", //(string, default: 'id_token'): The type of response desired from the OIDC provider.
