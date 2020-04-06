8f4bc4885ba4a6ce895dabbf3dece9a3ddc2d4d7
Niels Sandholt Busch <niels.busch@gmail.com>
2020-04-06 19:40:06 +0200

    Revert fix of begins changed, broke saving with correct date format

--- src/components/VisitForm.js
+++ src/components/VisitForm.js
@@ -335,9 +335,8 @@ class VisitForm extends Component<Props & { intl: intlShape }> {
 
   onBeginsChanged = (e, newValue, prevValue) => {
     const { ends, change, dispatch } = this.props;
-    if (newValue > prevValue) {
-      const difference = ends - prevValue;
-      dispatch(change("ends", newValue.getTime() + difference));
+    if (new Date(newValue) > new Date(ends)) {
+      dispatch(change("ends", newValue));
     }
   };
 }
